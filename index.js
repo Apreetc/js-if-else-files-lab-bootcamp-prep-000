47bb40deac0735f127c6c9cadfbfd10e710f9c70
Amanpreet Sethi <apreetc@gmail.com>
2017-10-10 14:34:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ if (name === "Susan") {
   console.log("The name is correct")
 }else {
   console.log("Expected the name to be Susan")}
+  if (height === "70")
+  {console.log("The height is correct")}
+  
 
 
 // Don't worry about this
